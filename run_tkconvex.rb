cbfb37409ba5c1cbe9e2e10558e4e85d73e9de08
Mansur <ele638@gmail.com>
2014-06-14 00:01:08 +0400

    Откат до эталона для подсчета строк
    #да, это дикий костыль

--- run_tkconvex.rb
+++ run_tkconvex.rb
@@ -1,15 +1,15 @@
+#!/usr/bin/env ruby
 require "./convex"
 require "./tk_drawer"
 
 TkDrawer.create
-
 fig = Void.new
 fig.draw
 begin
   while true
     fig = fig.add(R2Point.new)
     fig.draw
-    puts "S = #{fig.area}, P = #{fig.perimeter}, count = #{fig.inside_points}" #вывод ответа (исправлено)
+    puts "S = #{fig.area}, P = #{fig.perimeter}"
   end
 rescue EOFError
   puts "\nStop"
--- tk_drawer.rb
+++ tk_drawer.rb
@@ -19,18 +19,7 @@ module TkDrawer
   def TkDrawer.draw_line(p,q)
     TkcLine.new(CANVAS, x(p), y(p), x(q), y(q)) {fill("black")}    
   end
-  # рисование треугольника (добавлено)
-  def TkDrawer.draw_poly(a,b,c)
-	TkcPolygon.new(CANVAS, x(a), y(a), x(b), y(b), x(c), y(c), 'fill'=>'green')
-  end
-  # рисование окрестности (добавлено)
-   def TkDrawer.draw_line_1(p,q) 
-    TkcLine.new(CANVAS, x(p), y(p), x(q), y(q), 'width'=>'100', 'fill'=>'green' )    
-  end
-  # рисование кругов (добавлено)
-   def TkDrawer.draw_circle(p)
-    TkcOval.new(CANVAS, x(p) + 50, y(p) + 50, x(p) - 50, y(p) - 50, 'fill'=>"green")
-  end
+
   private
   # преобразование координат
   def TkDrawer.x(p)
@@ -51,20 +40,12 @@ end
 class Figure
   def draw
     TkDrawer.clean
-	TkDrawer.draw_poly(@@a,@@b,@@c)
-	TkDrawer.draw_line_1(@@a,@@b)
-	TkDrawer.draw_line_1(@@b,@@c)
-	TkDrawer.draw_line_1(@@a,@@c)
-	TkDrawer.draw_circle(@@a)
-	TkDrawer.draw_circle(@@b)
-	TkDrawer.draw_circle(@@c)
   end
 end
 class Point < Figure
   def draw
     super
     TkDrawer.draw_point(@p)
-
   end
 end
 class Segment < Figure
@@ -75,7 +56,7 @@ class Segment < Figure
 end
 class Polygon < Figure
   def draw
-    super 
+    super
     @points.size.times do 
       TkDrawer.draw_line(@points.last, @points.first)
       @points.push_last(@points.pop_first)
